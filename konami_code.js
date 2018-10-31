ee90fb9af4fde2725514f02523894c6fbeef72c8
asc5025 <albert.s.chun@gmail.com>
2018-10-31 09:12:31 -0400

    Done.

--- konami_code.js
+++ konami_code.js
@@ -13,4 +13,23 @@ const codes = [
 
 function init() {
   // your code here
+  let i = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    // console.log(key)
+    if (key === codes[i]) {
+      i++;
+      console.log(codes[i])
+      if (i === codes.length) {
+        alert('Hurray!');
+
+        i = 0;
+      }
+    } else {
+      i = 0;
+    }
+  })
+
+
 }
